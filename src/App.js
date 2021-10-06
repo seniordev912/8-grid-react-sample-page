4e584c2064a86cb3613ba4782d9d27f22fc583c6
seniordev912 <senior.dev.912@gmail.com>
2021-10-06 20:47:41 +0400

    feat: delete footer on App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from "./components/header";
 import Content1 from "./components/content1";
 import Content2 from "./components/content2";
-import Footer from "./components/footer"
+//import Footer from "./components/footer"
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <Content1 />
       <Content2 />
-      <Footer />
+//      <Footer />
     </div>
   );
 }
